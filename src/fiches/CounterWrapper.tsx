dd6d0ba674b296789921c54ed1b6f717318020f4
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-07-19 23:09:04 +0300

    Created Counter and CounterSettings, added isShowCounterSettings

--- src/fiches/CounterWrapper.tsx
+++ src/fiches/CounterWrapper.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import {useSelector} from "react-redux";
+import {CounterSettings} from "./CounterSettings/CounterSettings";
+import {Counter} from "./Counter/Counter";
+import {isShowCounterSettingsSelector} from "../state/selectors";
 
 
 export const CounterWrapper = () => {
+    const isShowCounterSettings = useSelector(isShowCounterSettingsSelector)
     return (
-            <>
-
-            </>
+        isShowCounterSettings ?
+            <CounterSettings/>:
+            <Counter/>
     );
 };
