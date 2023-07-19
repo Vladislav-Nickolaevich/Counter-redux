23cc7f94bc84b5b8e3bd4cd073459e6b6b51914b
Vladislav Nickolaevich <vlad_6521891@mail.ru>
2023-07-19 23:03:20 +0300

    Finished counter-reducer

--- src/state/counter-reducer.ts
+++ src/state/counter-reducer.ts
@@ -1,3 +1,9 @@
+const SHOW_COUNTER_SETTINGS = 'SHOW-COUNTER-SETTINGS'
+const INC_BTN = 'INC_BTN'
+const RESET_BTN = 'RESET-BTN'
+const SET_MIN_AND_MAX_VALUES = 'SET-MIN-AND-MAX-VALUES'
+
+type ActionType = showCounterSettingsACType | IncBtnACType | ResetBtnACType | SetMinAndMaxValuesACType
 
 export type initialStateType = {
     minValue: number
@@ -12,11 +18,46 @@ const initialState:initialStateType = {
     isShowCounterSettings: false,
 }
 
-export const counterReducer = (state:initialStateType= initialState, action: any): initialStateType => {
+export const counterReducer = (state:initialStateType= initialState, action: ActionType): initialStateType => {
     switch (action.type) {
-
+        case INC_BTN :
+            return {
+                ...state,
+                currentValue: state.currentValue + 1,
+            }
+        case SHOW_COUNTER_SETTINGS:
+            return {
+                ...state,
+                isShowCounterSettings: !state.isShowCounterSettings,
+            }
+        case SET_MIN_AND_MAX_VALUES:
+            return {
+                ...state,
+                minValue: action.minValue,
+                maxValue: action.maxValue,
+                currentValue: action.minValue
+            }
+        case RESET_BTN:
+            return{
+                ...state,
+                currentValue: state.minValue
+            }
         default:
             return state
     }
 }
+
+type IncBtnACType = ReturnType<typeof incBtnAC>
+export const incBtnAC = () => ({type: INC_BTN} as const)
+
+type ResetBtnACType = ReturnType<typeof resetBtnAC>
+export const resetBtnAC = () => ({type: RESET_BTN} as const)
+
+type showCounterSettingsACType = ReturnType<typeof showCounterSettingsAC>
+export const showCounterSettingsAC = () => ({type: SHOW_COUNTER_SETTINGS} as const) /// подумать над названием
+
+type SetMinAndMaxValuesACType = ReturnType<typeof setMinAndMaxValuesAC>
+export const setMinAndMaxValuesAC = ( minValue: number, maxValue: number) => (
+    {type: SET_MIN_AND_MAX_VALUES, minValue, maxValue } as const
+)
 
